8e16fa296782d55b38db43a5991e1ff85a95b0ce
WojciechPtas <wojciech.ptas@gmail.com>
2022-04-15 19:55:45 +0200

    virutal funkcje poll update i render

--- MK_SFML_Game/Scene.h
+++ MK_SFML_Game/Scene.h
@@ -5,15 +5,24 @@ class Scene
 public:
 	Scene();
 	~Scene();
-	/// <summary>
-	/// Metoda odpowiedzialna za dzia³anie sceny
-	/// </summary>
+	
+
+	// Metoda odpowiedzialna za dzia³anie sceny
 	virtual void run() = 0;
-	/// <summary>
-	/// Metoda wyci¹gaj¹ca wynik ze sceny (kto wygra³ albo jakie postacie zosta³y wybrane)
-	/// </summary>
-	/// <returns></returns>
+	
+	// Metoda wyci¹gaj¹ca wynik ze sceny (kto wygra³ albo jakie postacie zosta³y wybrane)
 	virtual std::string getResult() = 0;
+
+	virtual void pollEvents() = 0;
+
+
+	//updates
+	virtual void update() = 0;
+
+
+
+	// renders
+	virtual void render() = 0;
 
 private:
 
