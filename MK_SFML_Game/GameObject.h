9cf21c6c592d984052bc11500d3ef0bd01e1432b
MasterCoookie <jas.kocurek2@wp.pl>
2022-05-15 12:33:26 +0200

    lib includes changed to imports

--- MK_SFML_Game/GameObject.h
+++ MK_SFML_Game/GameObject.h
@@ -1,8 +1,8 @@
 #pragma once
-#include <iostream>
-#include <string>
-#include <unordered_map>
-#include <ranges>
+import <iostream>;
+import <string>;
+import <unordered_map>;
+import <ranges>;
 
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
