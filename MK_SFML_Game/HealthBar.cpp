dae87c5ef309be51d856ead1913cf237e8be502f
WojciechPtas <wojciech.ptas@gmail.com>
2022-05-31 14:41:15 +0200

    health bar right player

--- MK_SFML_Game/HealthBar.cpp
+++ MK_SFML_Game/HealthBar.cpp
@@ -8,11 +8,11 @@ HealthBar::HealthBar(bool isLeft)
 	this->healthShape->setSize({ 450.f, 50.f });
 	if (this->isLeft) {
 		this->healthShape->setPosition({ 730.f,50.f });
-		this->originalPos = sf::Vector2f(50.f, 50.f);
+		this->originalPos = sf::Vector2f(730.f, 50.f);
 	}
 	else {
 		this->healthShape->setPosition({ 1380.f,50.f });
-		this->originalPos = sf::Vector2f(1280 - 450, 50);
+		this->originalPos = sf::Vector2f(1380.f, 50.f);
 	}
 }
 
@@ -23,7 +23,11 @@ void HealthBar::render(sf::RenderWindow* win)
 
 void HealthBar::update(float currentHealth)
 {
-	if (this->isLeft) {
-		this->healthShape->setSize({ 450.f * currentHealth / 100,50.f });
+	this->healthShape->setSize({ 450.f * currentHealth / 100,50.f });
+	if (this->isLeft) {}
+	else {
+		sf::Vector2f newVec = this->originalPos;
+		newVec.x += 450.f-450.f * currentHealth / 100.f;
+		this->healthShape->setPosition(newVec);
 	}
 }
