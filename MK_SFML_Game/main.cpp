ad0f060ec96f5ea87f7bca7d100760ff9beddc96
WojciechPtas <wojciech.ptas@gmail.com>
2022-06-03 19:58:22 +0200

    deleted unnecessary include

--- MK_SFML_Game/main.cpp
+++ MK_SFML_Game/main.cpp
@@ -2,11 +2,14 @@
 
 #include "SceneManager.h"
 
-#include "AttackMovesMatrix.h"
+
 int main() {
 
-	SceneManager kumbat;
-	kumbat.run();
+	{
+		SceneManager kumbat;
+		kumbat.run();
+	}
+	
 	//sf::RenderWindow* win = new sf::RenderWindow(sf::VideoMode(1280, 960), "Patykovy Mordulec", sf::Style::Titlebar | sf::Style::Close);
 	//win->setFramerateLimit(30);
 	//LoadingScreen load(win, "char1", "char2");
